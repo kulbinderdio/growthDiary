828e0d57c870343584e5642f0a2caf3dee763846
kulbinder dio <kulbinderdio@gmail.com>
2021-06-22 16:26:41 +0100

    Updated to now have a longer term Goals page. Yearly, quarterly and monthly

--- diary.py
+++ diary.py
@@ -7,12 +7,14 @@ from datetime import date, datetime
 from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 from wtforms.fields import DateField
 from flask_sqlalchemy import SQLAlchemy
 import json
 from GoogleNews import GoogleNews
+from flask_migrate import Migrate
+
 googlenews = GoogleNews(period='7d',lang='en')
 weekdays = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
 
@@ -23,6 +25,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bootstrap=Bootstrap(app)
 datepicker(app)
 SECRET_KEY = os.urandom(32)
@@ -36,7 +39,7 @@ class Entry(db.Model):
     grateful = db.Column(db.Text())
 
     def __repr__(self):
-        return "<Title: {}>".format(self.goals)
+        return "<Date: {}>".format(self.date)
 
 class EntryForm(FlaskForm):
     edate = DateField('Date',format='%Y-%m-%d', id='datepicker', validators=[DataRequired()], default = date.today)
@@ -44,6 +47,54 @@ class EntryForm(FlaskForm):
     headline = StringField('Headline', render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100"})
     goals = TextAreaField('Goals',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
     notes = TextAreaField('Notes',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    submit = SubmitField('Save...')
+
+class Goal(db.Model):
+    year = db.Column(db.String(), unique=True, nullable=False, primary_key=True)
+    yearly =  db.Column(db.Text())
+    quarter1 = db.Column(db.Text())
+    quarter2 = db.Column(db.Text())
+    quarter3 = db.Column(db.Text())
+    quarter4 = db.Column(db.Text())
+
+    january = db.Column(db.Text())
+    february = db.Column(db.Text())
+    march = db.Column(db.Text())
+    april = db.Column(db.Text())
+    may = db.Column(db.Text())
+    june = db.Column(db.Text())
+    july = db.Column(db.Text())
+    august = db.Column(db.Text())
+    september = db.Column(db.Text())
+    october = db.Column(db.Text())
+    november = db.Column(db.Text())
+    december = db.Column(db.Text())
+
+    def __repr__(self):
+        return "<Year: {}>".format(self.year)
+
+class GoalsForm(FlaskForm):
+    year = SelectField('', choices=[('2021', '2021'), ('2022', '2022'), ('2023', '2023'), ('2024', '2024'), ('2025', '2025'), ('2026', '2026')])
+    yearly = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    quarter1 = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    quarter2 = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    quarter3 = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    quarter4 = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+
+    january = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    february = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    march = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    april = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    may = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    june = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    july = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    august = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    september = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    october = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    november = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    december = TextAreaField('Grateful',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+    notes = TextAreaField('Notes',render_kw={"class": "h6 mb-0 font-weight-bold text-gray-800 w-100","contenteditable":"true","aria-multiline":"true"})
+
     submit = SubmitField('Save...')
 
 @app.route("/", methods=["GET", "POST"])
@@ -74,7 +125,75 @@ def home():
             form.notes.data = entry.notes
             form.grateful.data = entry.grateful
             # form.edate=today
-    return render_template("newhome.html", form=form, data=news, weekday=weekdays[weekday])
+    return render_template("newhome.html", form=form, data=news, weekday=weekdays[weekday], goals_url="goals/"+str(datetime.now().year))
+
+
+@app.route("/goals/<year>", methods=["GET", "POST"])
+@app.route("/goals", defaults={'year': None}, methods=["POST"])
+def goals(year):
+    form = GoalsForm()
+
+    if form.validate_on_submit():
+        goal = Goal.query.filter_by(year=request.form.get("year")).first()
+        if goal == None:
+            goal = Goal(year=request.form.get("year"),yearly=request.form.get("yearly"),
+                         quarter1=request.form.get("quarter1"),quarter2=request.form.get("quarter2"),
+                         quarter3=request.form.get("quarter3"), quarter4=request.form.get("quarter4"),
+                         january=request.form.get("january"), february=request.form.get("february"),
+                         march=request.form.get("march"), april=request.form.get("april"),
+                         may=request.form.get("may"), june=request.form.get("june"),
+                         july=request.form.get("july"), august=request.form.get("august"),
+                         september=request.form.get("september"), october=request.form.get("october"),
+                         november=request.form.get("november"), december=request.form.get("december")
+                         )
+            db.session.add(goal)
+        else:
+            goal.yearly = request.form.get("yearly")
+            goal.quarter1=request.form.get("quarter1")
+            goal.quarter2 = request.form.get("quarter2")
+            goal.quarter3 = request.form.get("quarter3")
+            goal.quarter4 = request.form.get("quarter4")
+            goal.january = request.form.get("january")
+            goal.february = request.form.get("febuary")
+            goal.march = request.form.get("march")
+            goal.april = request.form.get("april")
+            goal.may = request.form.get("may")
+            goal.june = request.form.get("june")
+            goal.july = request.form.get("july")
+            goal.august = request.form.get("august")
+            goal.september = request.form.get("september")
+            goal.october = request.form.get("october")
+            goal.november = request.form.get("november")
+            goal.december = request.form.get("december")
+        db.session.commit()
+
+    else:
+        if not year:
+            year = datetime.now().year
+        goals = Goal.query.filter_by(year=year).first()
+        form.year.data = year
+        if goals:
+            form.yearly.data = goals.yearly
+            form.quarter1.data = goals.quarter1
+            form.quarter2.data = goals.quarter2
+            form.quarter3.data = goals.quarter3
+            form.quarter4.data = goals.quarter4
+            form.january.data = goals.january
+            form.february.data = goals.february
+            form.march.data = goals.march
+            form.april.data = goals.april
+            form.may.data = goals.may
+            form.june.data = goals.june
+            form.july.data = goals.july
+            form.august.data = goals.august
+            form.september.data = goals.september
+            form.october.data = goals.october
+            form.november.data = goals.november
+            form.december.data = goals.december
+
+    return render_template("goals.html", form=form)
+
+
 
 @app.route("/date/<datee>")
 def date(datee):
